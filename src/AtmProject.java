c0ddc14cb08c24378dd5e2f15c06235af36b12c9
Yusuf Taha Human <semt61@hotmail.com>
2023-04-26 22:55:36 +0300

    ysf1

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -9,6 +9,9 @@ public class AtmProject {
         System.out.println("Git öğrenmek çok kolay");
         //version4
 
+        System.out.println("yusuf branch'i kodlarını yazdı. ");
+        //version ysf1
+
 
     }
 
